21c460705114496b1188b557b8b30fcaad610f2c
VishalRothe <vishalrothe123@gmail.com>
2021-06-08 11:14:15 +0530

    bugFix

--- apps/stockTwit.py
+++ apps/stockTwit.py
@@ -12,11 +12,9 @@ def app():
 		r = requests.get(f"https://api.stocktwits.com/api/2/streams/symbol/{symbol}.json")
 
 	data = r.json()
-	empty = bool(data)
-	if empty==False:
-		st.error("enter a valid ticker")
-		raise Exception("Enter a valid ticker")
-
+	if data = {}:
+		st.error("invalid ticker")
+		raise Exception("invalid ticker")
 	for message in data['messages']:
 		st.image(message['user']['avatar_url'])
 		st.write(message['user']['username'])
