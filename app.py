110bb44b248cae4387aeb61b7cc28b4b82e5448f
VishalRothe <vishalrothe123@gmail.com>
2021-06-08 11:28:06 +0530

    bugFix

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from apps import home_page, prediction, stock_overview, calculator, news_module,
 
 app = MultiApp()
 
-st.set_page_config(page_title="PrediTicker", page_icon=None, layout='wide', initial_sidebar_state='collapsed', theme='Light')
+st.set_page_config(page_title="PrediTicker", page_icon=None, layout='wide', initial_sidebar_state='collapsed')
 
 app.add_app("Home", home_page.app)
 app.add_app("Stock Overview", stock_overview.app)
